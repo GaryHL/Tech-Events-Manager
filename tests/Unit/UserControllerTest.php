ad602d9165d8d06860e097af01c1fc15d75917c4
Matt <mattsandovalb@gmail.com>
2023-01-24 11:07:32 +0100

    Usercontrollertest

---
+++ tests/Unit/UserControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserControllerTest extends TestCase
+{
+
+    /**
+     * Test Index
+     */
+
+    public function Test_to_validate_db()
+    {
+        
+    }
+
+    public function CreateTest()
+    {
+        
+
+    }
+    public function StoreTest()
+    {
+
+    }
+    public function ShowTest()
+    {
+
+    }
+    public function EditTest()
+    {
+
+    }
+    public function UpdateTest()
+    {
+
+    }
+    public function DestroyTest()
+    {
+
+    }
+}
