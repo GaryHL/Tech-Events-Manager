c9ce83199e21f45aefde7f71409087bffec632e3
Matt <mattsandovalb@gmail.com>
2023-01-24 18:47:44 +0100

    fix login and register and some test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,26 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Validation\Rules;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
 
 class RegisteredUserController extends Controller
 {
     public function store(Request $request)
     {
-        return $request->validate([
-            'name' => ['required', 'string', 'max:255' ],
-            // 'lastname' => ['required', 'string', 'max:255'],
-            // 'username' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::default()],
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()]
         ]);
-        User::create([
+        $user = User::create([
             'name' => $request->name,
-            // 'lastname' => $request->lastname,
-            // 'username' => $request ->username,
             'email' => $request->email,
-            'password' => bcrypt($request -> password),
+            'password' => bcrypt($request->password),
         ]);
+        
 
-        return to_route('login')-> with('status', 'Account Created please Login');
+        //Auth::login($user);
+        return to_route('login')->with('status', 'Account created');
     }
 }
