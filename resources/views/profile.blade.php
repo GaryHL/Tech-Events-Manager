1ec0baba34798fdd6f3141e8dd2e9630a92d96a9
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-27 00:32:20 +0100

    change profile html

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,28 +1,116 @@
-<h1>Perfil</h1>
-<hr>
-<h3>User name:</h3>
-<hr>
-<h2>Crear nuevo evento</h2>
-<form action='event/post' method="post" enctype="multipart/form-data">
-    @csrf
-    <input type='text' name='title' placeholder="title" required />
-    <input type='text' name='description' placeholder="description" required />
-    <input type='file' name='url_img' placeholder="imagen" >
-    {{-- <input type='text' name='url_img' placeholder="url" required /> --}}
-    <input type='test' name='max_participants' placeholder="Max-participants" required />
-    {{-- <input type='number' name='outstanding' placeholder="Outstanding" required /> --}}
-    <label > Outsanding </label><select name='outstanding' required>
-        <option value="yes">si</option>
-        <option value="no">no</option>
-    </select>
-    <input type='text' name='link' placeholder="link" required />
-    <input type='date' name='fecha' placeholder="Fecha" required />
-    <input type='time' name='hora' placeholder="Hora" required />
-    <button type='submit'> pull</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
 
-<a href="home">volver a home</a>
-<form action='/logout' method='post'>
-    @csrf
-    <button type='sumbit'>Logout</button>
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <h1 class="title">Perfil</h1>
+    <br>
+    <p>Si no eres administrador porfavor no crees un nuevo evento</p>
+    <br>
+    <h2>Crear nuevo evento</h2>
+    <br>
+    <br>
+    <form action='event/post' method="post" enctype="multipart/form-data" class="form_edit">
+        @csrf
+        <div class="container_inputs">
+            <div class="group">
+                <input type='text' name='title' required class="input" />
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Titulo</label>
+            </div>
+            <div class="group">
+                <input type='text' name='description' required class="input" />
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Descripción</label>
+            </div>
+            <div class="group">
+                <p style="opacity:0.8">Img:</p>
+                <input type='file' name='url_img' required class="input" />
+                <span class="highlight"></span>
+                <span class="bar"></span>
+            </div>
+            <div class="group">
+                <input type='number' name='max_participants' required class="input" />
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Max-participants</label>
+            </div>
+
+            <div class="group">
+                <p style="opacity:0.8">Relevante?</p>
+                <br>
+                <select name='outstanding' required>
+                    <option value="yes">si</option>
+                    <option value="no">no</option>
+                </select>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+            </div>
+
+            <div class="group">
+                <input type='text' name='link' required class="input" />
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Link de la reunión</label>
+            </div>
+
+            <div class="group">
+                <p style="opacity:0.8">Fecha:</p>
+                <br>
+                <input type='date' name='fecha' placeholder="Fecha" required />
+                <span class="highlight"></span>
+                <span class="bar"></span>
+            </div>
+            <div class="group">
+                <p style="opacity:0.8">Hora:</p>
+                <br>
+                <input type='time' name='hora' placeholder="Hora" required />
+                <span class="highlight"></span>
+                <span class="bar"></span>
+            </div>
+        </div>
+        <button type='submit' class="btn_green"> Crear evento</button>
+    </form>
+        <form action='/logout' method='post'>
+        @csrf
+        <button type='sumbit' class="btn_delete">Cerrar sesión</button>
+    </form>
+
+
+    <nav class="navbar">
+        <a href="/home">
+            <lord-icon src="https://cdn.lordicon.com/gmzxduhd.json" trigger="hover" class="iconNav"
+                colors="primary:#ffffff,secondary:#ffffff">
+            </lord-icon>
+        </a>
+        <a href="/events">
+            <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="hover" class="iconNav"
+                colors="primary:#ffffff,secondary:#ffffff">
+            </lord-icon>
+        </a>
+        <a href="profile">
+            <lord-icon src="https://cdn.lordicon.com/imamsnbq.json" trigger="hover" class="iconNav"
+                colors="primary:#ffffff,secondary:#ffffff">
+            </lord-icon>
+        </a>
+        <a href="/events/tickets/myTickets">
+            <lord-icon src="https://cdn.lordicon.com/cjieiyzp.json" colors="primary:#ffffff,secondary:#ffffff"
+                class="iconNav" trigger="hover"> </lord-icon>
+        </a>
+    </nav>
+    <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
+
+</body>
+
+</html>
