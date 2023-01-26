446fc749f058e9495dccdb1449d47cf0f5696610
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-27 00:32:54 +0100

    events html

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -5,24 +5,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Search</title>
+
+
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link href="{{ asset('css/events.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Events</h1>
-    <hr>
-    <div>
-        <p>Lista de eventos</p>
-        <ul>
+    <h1 class="title">Events</h1>
+    <div class="container_event_list">
+        <p>Lista de eventos:</p>
+        <ul class="list">
             @foreach ($events as $event)
                 <li>
-                    <a href="/events/{{ $event->id }}">{{ $event->title }}</a>
+                    <a href="/events/{{ $event->id }} ">
+                        <div class="card_event">
+                            <img src={{ $event->url_img }} alt="" class="img_card">
+                            <div class="info_event">
+                                <h4 class="title_event">{{ $event->title }}</h4>
+                                <div class="place">
+                                    <p class="date_event">{{ $event->fecha }}</p>
+                                    <p class="description_event">{{ $event->description }}</p>
+                                </div>
+                            </div>
+                            <div class="container_icon">
+                                <p class="status">10/100</p><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                    <path
+                                        d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </a>
                 </li>
             @endforeach
         </ul>
     </div>
-    <hr>
-    <a href="/home"> volver a home</a>
+    <nav class="navbar">
+        <a href="/home">
+            <lord-icon src="https://cdn.lordicon.com/gmzxduhd.json" trigger="hover" class="iconNav"
+                colors="primary:#ffffff,secondary:#ffffff">
+            </lord-icon>
+        </a>
+        <a href="/events">
+            <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="hover" class="iconNav"
+            colors="primary:#ffffff,secondary:#ffffff">
+        </lord-icon>
+        </a>
+        <a href="/profile">
+            <lord-icon src="https://cdn.lordicon.com/imamsnbq.json" trigger="hover" class="iconNav"
+                colors="primary:#ffffff,secondary:#ffffff">
+            </lord-icon>
+        </a>
+        <a href="/events/tickets/myTickets">
+            <lord-icon src="https://cdn.lordicon.com/cjieiyzp.json" colors="primary:#ffffff,secondary:#ffffff"
+                class="iconNav" trigger="hover"> </lord-icon>
+        </a>
+    </nav>
+
+    <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
 </body>
 
 </html>
